6459ea32c7d5276fd04c72f48fbbe1e3f450ba54
no7mac <44577325+no7macs@users.noreply.github.com>
2020-01-28 20:07:35 -0700

    routine push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
-import os, shutil
+import os, shutil, time
 from subprocess import call
 
 filename = str()
@@ -11,7 +11,7 @@ def select_file():
     print(root.filename)
 
     root.currfile.config(text = "Current File Selected:" + root.filename)
-    root.filestatus.config(text = "File Status: Has Been Converted")
+    root.filestatus.config(text = "File Status: Has Not Been Converted")
 
 def save_location():
     root.savelocation = filedialog.askdirectory(initialdir = "./", title = "Select Save Location")
@@ -21,66 +21,68 @@ def convert():
 
     try:
         with open(root.savelocation + os.path.basename(root.filename)) as f:
-            print(f.readlines())
-            # Do something with the file
+            root.filestatus.config(text = "ERR. Duplicate File In Save Location")
+            root.mainloop()
     except IOError:
-        root.filestatus.config(text = "ERR. Duplicate File In Save Location")
-        root.mainloop()
+        root.filestatus.config(text = "Converting...")
 
-    programdir = os.path.dirname(os.path.abspath(__file__))
-    print(programdir)
+        programdir = os.path.dirname(os.path.abspath(__file__))
+        print(programdir)
 
-    if root.filename == str(""):
-        root.filestatus.config(text = "File Converted: ERR, No File Selected")    
-    else:
-        shutil.copyfile(root.filename, programdir + "/repo/" + os.path.basename(root.filename))
-        os.chdir(programdir + "/repo")
-        print(os.getcwd())
-        os.system("node" " krunkerToWavefront.js " + os.path.basename(root.filename))
-        root.filestatus.config(text = "File Status: File Has Not Been Converted")
+        if root.filename == str(""):
+            root.filestatus.config(text = "File Converted: ERR, No File Selected") 
+            root.mainloop()   
+        else:
+            shutil.copyfile(root.filename, programdir + "/repo/" + os.path.basename(root.filename))
+            os.chdir(programdir + "/repo")
+            print(os.getcwd())
+            os.system("node" " krunkerToWavefront.js " + os.path.basename(root.filename))
+            root.filestatus.config(text = "File Status: File Has Been Converted")
 
-        filecontent = []
-        filecontent = os.listdir("./wavefront")
+            filecontent = []
+            filecontent = os.listdir("./wavefront")
 
-        for i in filecontent:
-            if i == ("textures"):
-                pass
-            else: 
-                os.rename("./wavefront/" + i , root.savelocation + "/" + i)
-    os.remove(programdir + "/repo/" + os.path.basename(root.filename))
+            for i in filecontent:
+                if i == ("textures"):
+                    pass
+                else: 
+                    os.rename("./wavefront/" + i , root.savelocation + "/" + i)
+            
+            os.remove(programdir + "/repo/" + os.path.basename(root.filename))
 
 if __name__ == "__main__":
     root = Tk()
     root.title("Krunker file converter GUI")
     root.geometry("525x275")
     root.resizable(0,0)
+    root.configure(bg='#393952')
     #root.tk.call('wm', 'iconphoto', root._w, PhotoImage(file='Data/Images/favicon.png'))
 
-    root.fileselectandconvert = Frame(root)
+    root.fileselectandconvert = Frame(root, bg='#393952')
     root.fileselectandconvert.pack(side = BOTTOM)
 
-    root.otherinfo = Frame(root)
+    root.otherinfo = Frame(root, bg='#393952')
     root.otherinfo.pack(side = LEFT)
 
     #file select and convert buttons
 
-    root.Convertfile = Button(root.fileselectandconvert, text = "CONVERT", command = convert, width = 525, height = 5)
+    root.Convertfile = Button(root.fileselectandconvert, text = "CONVERT", command = convert, width = 525, height = 5, bg='#393952', fg='#fafafa', activebackground='#f06f51', activeforeground='#fafafa')
     root.Convertfile.pack(side = BOTTOM)
 
-    root.Selectfilebutton  = Button(root.fileselectandconvert, text = "Select File", command = select_file, width = 525, height = 2)
+    root.Selectfilebutton  = Button(root.fileselectandconvert, text = "Select File", command = select_file, width = 525, height = 2, bg='#393952', fg='#fafafa', activebackground='#f06f51', activeforeground='#fafafa')
     root.Selectfilebutton.pack(side = BOTTOM)
 
-    root.Savefilelocation = Button(root.fileselectandconvert, text = "Save Location", command = save_location, width = 525, height = 2)
+    root.Savefilelocation = Button(root.fileselectandconvert, text = "Save Location", command = save_location, width = 525, height = 2, bg='#393952', fg='#fafafa', activebackground='#f06f51', activeforeground='#fafafa')
     root.Savefilelocation.pack(side = BOTTOM)
 
     #other info that people might want
-    root.currfile = Label(root.otherinfo, text = "Current File Selected:")
+    root.currfile = Label(root.otherinfo, text = "Current File Selected:", bg='#393952', fg='#fafafa')
     root.currfile.pack(anchor = W)
 
-    root.savelocationtext = Label(root.otherinfo, text = "Save Location Selected:")
+    root.savelocationtext = Label(root.otherinfo, text = "Save Location Selected:", bg='#393952', fg='#fafafa')
     root.savelocationtext.pack(anchor = W)
 
-    root.filestatus = Label(root.otherinfo, text = "File Status:")
+    root.filestatus = Label(root.otherinfo, text = "File Status:", bg='#393952', fg='#fafafa')
     root.filestatus.pack(anchor = W)
 
     root.mainloop()
